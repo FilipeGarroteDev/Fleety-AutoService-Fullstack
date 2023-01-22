69dbe0ab3718f58bf58524831735c1ccc475943b
Filipe Garrote <filipe.garrote@gmail.com>
2023-01-22 15:30:31 -0300

    feat: added click on options menu to expand or collapse

--- src/pages/Dashboard/ProductPage/index.jsx
+++ src/pages/Dashboard/ProductPage/index.jsx
@@ -3,10 +3,8 @@ import styled from 'styled-components';
 import { FaChevronDown, FaChevronUp } from 'react-icons/fa';
 
 export default function ProductPage() {
-  const [isClicked, setIsClicked] = useState(false);
-
   return (
-    <Wrapper isClicked={isClicked}>
+    <Wrapper>
       <ProductBanner>
         <img
           src="https://saboreiaavida.nestle.pt/sites/default/files/styles/receita_card_620x560/public/pictures/e10d8b86-55e2-11e4-b7b1-d4ae52be23e7.png?h=80accfed&itok=ZvgUCzxd"
@@ -18,21 +16,29 @@ export default function ProductPage() {
         Salada fresca com folhas diversas, incluindo alface crespa, rúcula e alface americana, além de salmão desfiado,
         azeitonas e muçarela de búfala.
       </h2>
-      <div>
-        <div className="closed" onClick={() => setIsClicked(!isClicked)}>
-          <h3>Deseja retirar algum item?</h3>
-          {isClicked ? <FaChevronUp /> : <FaChevronDown />}
-        </div>
-        <div className="opened">
-          <textarea placeholder="Digite aqui o que deseja retirar..." />
-        </div>
-      </div>
+      <OptionsMenu />
       {/* <div>
         <h3>Adicionais</h3>
         <FaChevronDown />
       </div> */}
       <footer></footer>
     </Wrapper>
+  );
+}
+
+function OptionsMenu() {
+  const [isClicked, setIsClicked] = useState(false);
+
+  return (
+    <OptionsBoxStyle isClicked={isClicked}>
+      <div className="closed" onClick={() => setIsClicked(!isClicked)}>
+        <h3>Deseja retirar algum item?</h3>
+        {isClicked ? <FaChevronUp /> : <FaChevronDown />}
+      </div>
+      <div className="opened">
+        <textarea placeholder="Digite aqui o que deseja retirar..." />
+      </div>
+    </OptionsBoxStyle>
   );
 }
 
@@ -44,58 +50,6 @@ const Wrapper = styled.main`
   > h2 {
     font-size: 26px;
     color: #d9d9d9;
-  }
-
-  > div {
-    width: 100%;
-    height: auto;
-    padding: 0 15px;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    background-color: #655d5d;
-    border-radius: 6px;
-    box-shadow: 2px 0 10px 0 rgba(0, 0, 0, 0.3);
-    color: #ffffff;
-    font-size: 20px;
-    font-weight: 700;
-    margin-top: 25px;
-    transition: all 0.3s;
-    overflow: hidden;
-
-    > .closed {
-      width: 100%;
-      height: 55px;
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-    }
-
-    > .opened {
-      display: flex;
-      height: ${(props) => (props.isClicked ? '205px' : '0')};
-      width: 100%;
-      transition: all 1s;
-
-      > textarea {
-        width: 100%;
-        height: 80%;
-        background-color: #655d5d;
-        border-radius: 15px;
-        border: 1px solid #d9d9d9;
-        outline: none;
-        resize: none;
-        font-size: 20px;
-        color: #d9d9d9;
-        padding: 15px;
-
-        ::placeholder {
-          font-size: 16px;
-          color: #d9d9d9;
-          font-style: italic;
-        }
-      }
-    }
   }
 `;
 
@@ -140,5 +94,57 @@ const ProductBanner = styled.header`
     font-weight: 700;
     bottom: 10px;
     left: calc(50% - 150px);
+  }
+`;
+
+const OptionsBoxStyle = styled.section`
+  width: 100%;
+  height: auto;
+  padding: 0 15px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  background-color: #655d5d;
+  border-radius: 6px;
+  box-shadow: 2px 0 10px 0 rgba(0, 0, 0, 0.3);
+  color: #ffffff;
+  font-size: 20px;
+  font-weight: 700;
+  margin-top: 25px;
+  transition: all 0.3s;
+  overflow: hidden;
+
+  > .closed {
+    width: 100%;
+    height: 55px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  > .opened {
+    display: flex;
+    height: ${(props) => (props.isClicked ? '205px' : '0')};
+    width: 100%;
+    transition: all 1s;
+
+    > textarea {
+      width: 100%;
+      height: 80%;
+      background-color: #655d5d;
+      border-radius: 15px;
+      border: 1px solid #d9d9d9;
+      outline: none;
+      resize: none;
+      font-size: 20px;
+      color: #d9d9d9;
+      padding: 15px;
+
+      ::placeholder {
+        font-size: 16px;
+        color: #d9d9d9;
+        font-style: italic;
+      }
+    }
   }
 `;
